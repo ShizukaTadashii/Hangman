54d8d302041226e6c3c99c0dd352b7f1b0941f09
ShizukaTadashii <shizukatadashii@gmail.com>
2014-05-14 01:55:39 -0400

    Added: >A list that displays to the user what incorrect letters have already been guessed
    Changed:
    >is_correct() now only checks for correctness, which should have been it's job in the first
    As such, I moved the functionality of updating the number of wrong guesses and the updating
    of the blanks to their own functions.
    
    Things to Add:
    >Reading in a "Library"(.txt file) of words(perhaps phrases, too) and randomising which
    word becomes the word to guess.

--- Hangman.h
+++ Hangman.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <stdio.h> //printf
 
 #define MAX_GUESSES 7
@@ -13,19 +14,24 @@ class Hangman{
 		char get_guess(void);
 		std::string get_guesses(void);
 		std::string get_word(void);
+		std::vector <char> get_already_guessed();
 		void set_guess(char guess);
 		void set_num_wrong(int num_wrong);
 		void set_word(std::string word);
+		void push_back_already_guessed();
 
 		void init_guesses_blank();
 		void init_man_blank();
 		void print_guesses();
 		void print_end_game();
 		void print_man();
+		void print_already_guessed();
 
 		void ask_guess(void);
 		bool is_correct();
 		bool is_completed();
+		void update_guesses();
+		void update_num_wrong();
 
 	private:
 		int m_num_wrong;
@@ -33,4 +39,5 @@ class Hangman{
 		std::string m_guesses;
 		std::string m_word;
 		std::string m_man[7];
+		std::vector <char> m_already_guessed;
 };
--- hangman.cpp
+++ hangman.cpp
@@ -1,17 +1,23 @@
 #include "Hangman.h"
 
 
-int main(){
+int main(int argc, char**argv){
+
+	//FILE* f = fopen("defaultLibrary.txt", "r");
+
 	Hangman hangman;
 	while(hangman.is_completed() == false){
 		if(hangman.get_num_wrong() == MAX_GUESSES) break;
 		hangman.print_man();
+		hangman.update_guesses();
 		hangman.print_guesses();
+		hangman.print_already_guessed();
 		hangman.ask_guess();
-		hangman.is_correct();
+		hangman.update_num_wrong();
 	}
 	hangman.print_man();
 	hangman.print_guesses();
+	hangman.print_already_guessed();
 	hangman.print_end_game();
 
 	return 0;
@@ -22,7 +28,7 @@ int main(){
 Hangman::Hangman(){
 	this->m_num_wrong = 0;
 	this->m_guess = ' ';
-	m_word= "llama";
+	set_word("llama");
 	init_guesses_blank();
 	init_man_blank();
 }
@@ -33,9 +39,13 @@ int Hangman::get_num_wrong(void){ return m_num_wrong; }
 char Hangman::get_guess(void){ return m_guess; }
 std::string Hangman::get_guesses(void){ return m_guesses; }
 std::string Hangman::get_word(void){ return m_word; }
+std::vector <char> Hangman::get_already_guessed(){ return m_already_guessed;}
 void Hangman::set_guess(char guess){this->m_guess = guess; }
 void Hangman::set_num_wrong(int num_wrong){this->m_num_wrong = num_wrong; }
 void Hangman::set_word(std::string word){ m_word= word; }
+void Hangman::push_back_already_guessed(){
+	m_already_guessed.push_back(get_guess());
+}
 
 
 
@@ -83,18 +93,13 @@ void Hangman::print_end_game(){
 
 void Hangman::print_man(){
 	switch(get_num_wrong()){
-		case MAX_GUESSES: {
-			m_man[5][9] = '\\';
-		}
-		case (MAX_GUESSES-1): m_man[5][7] = '/';
-		case (MAX_GUESSES-2): m_man[4][8] = '|';
-		case (MAX_GUESSES-3): m_man[3][9] = '\\';
-		case (MAX_GUESSES-4): m_man[3][7] = '/';
-		case (MAX_GUESSES-5): m_man[3][8] = '|';
-		case (MAX_GUESSES-6): {
-				m_man[2][8] = 'O';
-				break;
-		}
+		case MAX_GUESSES: 		{m_man[5][9] = '\\'; break;}
+		case (MAX_GUESSES-1): 	{m_man[5][7] = '/'; break;}
+		case (MAX_GUESSES-2): 	{m_man[4][8] = '|'; break;}
+		case (MAX_GUESSES-3): 	{m_man[3][9] = '\\'; break;}
+		case (MAX_GUESSES-4): 	{m_man[3][7] = '/'; break;}
+		case (MAX_GUESSES-5): 	{m_man[3][8] = '|'; break;}
+		case (MAX_GUESSES-6): 	{m_man[2][8] = 'O'; break;}
 		default: break;
 	}
 	for(int i = 0; i <= NUM_HANGMAN_COLS; i++){
@@ -104,8 +109,19 @@ void Hangman::print_man(){
 			std::cout << m_man[i] << std::endl;
 		}
 	}
-
 }
+
+
+
+void Hangman::print_already_guessed(){
+	std::cout << std::endl;
+	std::cout << "List of Wrong Guesses: ";
+	for(int i = 0; i < get_already_guessed().size(); i++){
+		std::cout << get_already_guessed()[i] << ", ";
+	}
+	std::cout << std::endl;
+}
+
 
 
 void Hangman::ask_guess(void){
@@ -113,20 +129,17 @@ void Hangman::ask_guess(void){
 	printf("Please enter a letter to guess:(%d remaining) ", MAX_GUESSES - get_num_wrong());
 	std::cin >> us_guess;
 	set_guess(us_guess);
+	if(!is_correct()){
+		push_back_already_guessed();
+	}
 }
 
 
 
 bool Hangman::is_correct(){
 	if( m_word.find(get_guess()) != std::string::npos ){
-		for(int i = 0; i < get_word().size(); i++ ){
-			if(get_word()[i] == get_guess() ){
-				m_guesses[i] = get_guess();
-			}
-		}
 		return true;
 	}else{
-		m_num_wrong++;
 		return false;
 	}
 }
@@ -136,6 +149,27 @@ bool Hangman::is_correct(){
 bool Hangman::is_completed(){
 	if( get_guesses().compare(get_word()) == 0){
 		return true;
+	}else{
+		return false;
 	}
-	return false;
+}
+
+
+
+void Hangman::update_guesses(){
+	if(is_correct() == true){
+		for(int i = 0; i < get_word().size(); i++ ){
+			if(get_word()[i] == get_guess() ){
+				m_guesses[i] = get_guess();
+			}
+		}
+	}
+}
+
+
+
+void Hangman::update_num_wrong(){
+	if(is_correct() == false){
+		m_num_wrong++;
+	}
 }
